ff802762bd6538b7fb01525954a5bb2daa0af925
77035338 <MY_NAME@example.com>
2021-12-04 15:00:50 -0500

    how to make slowly disappear?

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const animatedButton = document.querySelector(".start"); 
-animatedButton.addEventListener('click', () => {
-    animatedButton.style.opacity = "0";
-    animatedButton.style.pointerEvents = "none";
+//variables
+const start = document.querySelector(".start");
+const wrapper = document.querySelector("header");
+const title = document.querySelector("h1");
+//logic
+start.addEventListener('click', () => {
+    wrapper.style.display = "none";
 });

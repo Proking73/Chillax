0dbc1942afae9f9c30a36e7f0a594514c7bffbe5
77035338 <MY_NAME@example.com>
2021-12-05 15:09:42 -0500

    animtion fadeIn from choices

--- script.js
+++ script.js
@@ -2,10 +2,20 @@
 const start = document.querySelector(".start");
 const wrapper = document.querySelector("header");
 const title = document.querySelector("h1");
+const choices = document.querySelector(".choices");
+const choiceItems = document.querySelector(".choice-item");
+//animations
+const fadeOut = "fadeOut 0.75s ease-out";
+const fadeIn = "fadeIn 0.5s ease-in";
 //logic
 function hide() {
-    wrapper.style.animation = "fadeOut 0.75s ease-out";
+    wrapper.style.animation = fadeOut;
 }
 wrapper.addEventListener('animationend', () => {
     wrapper.style.display = "none";
+    choices.style.display = "flex";
+    choices.style.animation = fadeIn;
+});
+choices.addEventListener('animationend', () => {
+    choices.style.opacity = "1";
 });

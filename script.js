d129121a02f21fbf1c55d585f0766d5ff8bb89d5
77035338 <MY_NAME@example.com>
2021-12-05 14:26:01 -0500

    choices

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const start = document.querySelector(".start");
 const wrapper = document.querySelector("header");
 const title = document.querySelector("h1");
 //logic
-start.addEventListener('click', () => {
+function hide() {
+    wrapper.style.animation = "fadeOut 0.75s ease-out";
+}
+wrapper.addEventListener('animationend', () => {
     wrapper.style.display = "none";
 });

bfa8ca78276514be35981f7d768dd9c9ed098567
77035338 <MY_NAME@example.com>
2021-12-24 10:44:38 -0500

    braeak

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const wrapper = document.querySelector("header");
 const title = document.querySelector("h1");
 const choices = document.querySelector(".choices");
 const choiceItems = document.getElementsByClassName("choice-item");
-const choiceItem1 = document.querySelector(".choice-item1");
-const choiceItem2 = document.querySelector(".choice-item3");
-const choiceItem3 = document.querySelector(".choice-item3");
+const choiceItem1 = choiceItems[0];
+const choiceItem2 = choiceItems[1];
+const choiceItem3 = choiceItems[2].children.;
 //animations
 const fadeOut = "fadeOut 0.75s ease-out";
 const fadeIn = "fadeIn 0.5s ease-in";
@@ -24,6 +24,3 @@ wrapper.addEventListener('animationend', () => {
 choices.addEventListener('animationend', () => {
     choices.style.opacity = "1";
 });
-for (let x = 0; x < choiceItems.length; x++) {
-    console.log(choiceItems[x])
-}
